561cdc95aee9648044e746b141c6b8928b71b705
Martin Chaplin <martin.chaplin44@gmail.com>
2019-08-16 19:06:54 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,13 +2,10 @@ def find_element_index(array, value_to_find)
 counter=0 
  while array[counter]
    if array[counter]==value_to_find
-     counter
+     return counter
    end
- 
  counter+=1
- 
  end
-
 end
 
 def find_max_value(array)
